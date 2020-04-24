8faa71eb2af7a6c1839950c7deca5732520e5679
Michael Suter <suterm@hartwick.edu>
2020-04-23 22:56:46 -0400

    Assignment 12 finished

--- Assignment12regez.py
+++ Assignment12regez.py
@@ -3,6 +3,20 @@
 # 4/23/20
 import re
 
+print("""
+    Type 1 to check if your input has a 'q'
+    Type 2 to check if your input contains 'the'
+    Type 3 to check if your input has a '*'
+    Type 4 to check if your input contains a digit
+    Type 6 to check if your input contains a period
+    Type 6 to check if your input has at least 2 consecutive vowels
+    Type 7 to check if your input contains white space
+    Type 8 to check if your input has any letters that repeat three times
+    Type 9 to check if your input starts with Hello
+    Type 10 to check if your input contains an email address 
+    Type 11 to quit
+    """)
+
 
 def menu():
     answer = int(input(">"))
@@ -27,7 +41,7 @@ def menu():
     elif answer == 3:
         print("You chose to check if the string contains a *, This will return True if it is, else it will return False. ")
         response = input("Enter your string>")
-        if bool(re.match(* response)):
+        if bool(re.match('\*', response)):
             print(True)
         else:
             print(False)
@@ -35,7 +49,7 @@ def menu():
     elif answer == 4:
         print("You chose to check if the string contains a digit, This will return True if it is, else it will return False")
         response = input("Enter your string>")
-        if bool(re.match("12345678910", response)):
+        if bool(re.match(r'\d', response)):
             print(True)
         else:
             print(False)
@@ -43,7 +57,7 @@ def menu():
     elif answer == 5:
         print("You chose to check if the string contains a period '.' , This will return True if it is, else it will return False.")
         response = input("Enter your string>")
-        if bool(re.match(".", response)):
+        if bool(re.search('\.', response)):
             print(True)
         else:
             print(False)
@@ -51,7 +65,7 @@ def menu():
     elif answer == 6:
         print("You chose to check if the string contains 2 consecutive vowels, This will return True if it is, else it will return False.")
         response = input("Enter your string>")
-        if response is bool(re.search("aeiou", response)):
+        if response is bool(re.search(r'[aeiou]{2}', response)):
             print(True)
         else:
             print(False)
@@ -83,7 +97,7 @@ def menu():
     elif answer == 10:
         print("You chose to check if the string is an email address, This will return True if it is, else it will return False.")
         response = input("Enter your string>")
-        if response is bool(re.search('.com', response)):
+        if response is bool(re.search('.com', response)):       # stumped on this and 9, I make no excuse
             print(True)
         else:
             print(False)
